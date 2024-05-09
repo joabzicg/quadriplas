46c771e45b3fa1a015b2eda6b91e1fcd26ac3f46
Joab da Silva <joab.silva@estudante.ufcg.edu.br>
2024-05-09 20:43:05 -0300

    aa

--- quadriplas.py
+++ quadriplas.py
@@ -1,5 +1,14 @@
-#aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-#OPAA
-#32340io0r0r0
-#sdasd
-#vamo lá então
+import numpy as np
+
+# Defina sua matriz
+matriz = np.array([[1, 2],
+                   [3, 4]])
+
+# Calcule a matriz inversa
+matriz_inversa = np.linalg.inv(matriz)
+
+print("Matriz original:")
+print(matriz)
+
+print("\nMatriz inversa:")
+print(matriz_inversa)
