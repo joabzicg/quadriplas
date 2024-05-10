2e7a5f2fe44dbbec28a505785ec7caab2f4b131e
joabzicg <joab.silva@estudante.ufcg.edu.br>
2024-05-10 09:29:27 -0300

    as

--- Componentes/linha.py
+++ Componentes/linha.py
@@ -4,10 +4,10 @@ import numpy as np
 w = 2*np.pi*60 
 
 #MATRIZES LINHA DE TRANSMISSÃO
+
 def M_Linha(Linha):
     #Admitância Y da linha (Y1=Y2)
-    Y = 1/(1/1j*w*(Linha[2]/2))
-
+    Y = 1/(1/(1j*w*(Linha[2]/2)))
     #Impedância Z da linha
     Z = Linha[0]+(1j*w*Linha[1])
     #Calculando matriz ABCD
