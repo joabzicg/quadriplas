aaf07830c542a1e9091c899328faf9bfcee1df69
larissamnc <lmarissa168@gmail.com>
2024-05-10 13:16:46 -0300

    Update CARGAEMATRIZDERIVAÇAO.py

--- Componentes/CARGAEMATRIZDERIVAÇAO.py
+++ Componentes/CARGAEMATRIZDERIVAÇAO.py
@@ -17,18 +17,18 @@ def Carga (Z):
 
     return matriz
 
-    #matriz carga derivada
-    def CargaDerivada(Z):
-        A = 1
-        B = 0
-        C = 1/Z
-        D = 1
+#matriz carga derivada
+def CargaDerivada(Z):
+    A = 1
+    B = 0
+    C = 1/Z
+    D = 1
 
-        matriz = np.array(
-            [
-                [A,B],
-                [C,D]
-            ]
-        )
+    matriz = np.array(
+        [
+            [A,B],
+            [C,D]
+        ]
+    )
 
-        return matriz 
+    return matriz 
