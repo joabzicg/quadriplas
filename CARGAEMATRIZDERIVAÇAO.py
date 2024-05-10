cdf320d1a446fb415352d1e79bda5fc6b4f49cd6
larissamnc <lmarissa168@gmail.com>
2024-05-09 21:53:04 -0300

    Add files via upload

---
+++ CARGAEMATRIZDERIVAÇAO.py
@@ -0,0 +1,34 @@
+import numpy as np
+import cmath 
+
+#função carga
+def Carga (Z):
+    A = 1
+    B = Z
+    C = 0
+    D = 1
+
+    matriz = np.array(
+        [
+            [A,B],
+            [C,D]
+        ]
+    )
+
+    return matriz
+
+    #matriz carga derivada
+    def CargaDerivada(Z):
+        A = 1
+        B = 0
+        C = 1/Z
+        D = 1
+
+        matriz = np.array(
+            [
+                [A,B],
+                [C,D]
+            ]
+        )
+
+        return matriz 
