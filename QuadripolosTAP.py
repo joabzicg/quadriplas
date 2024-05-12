05e2cd3b9b0adabf1e4341fd7eb5c435053c5c55
Angeloamb <angeloantonio0553@gmail.com>
2024-05-11 22:44:46 -0300

    Update QuadripolosTAP.py

--- QuadripolosTAP.py
+++ QuadripolosTAP.py
@@ -167,7 +167,7 @@ Fim_daL_Linha = Associar_Matriz_em_Cascata (matriz8, Z3)
 
 #CALCULANDO TENSÃO E CORRENTE NAS CARGAS
 #Encontrando Vc e IC em Z3
-print("o=> Tensão e corrente na carga Z3:")
+print("o=> Tensão e corrente em rms na carga Z3:")
 Vg = 69e+3 #tensão na entrada do quadripolo
 Vc = (Vg*Zc3)/(matriz8[0][0]*Zc3 + matriz8[0][1]) #Tensão na carga 3 
 Ic = Vc/Zc3 #Corrente na carga 3
@@ -187,7 +187,7 @@ print(f"Vcarga3 = {absVc:.2f} ∠ {angVc:.2f}° V")
 print(f"Icarga3 = {absIc:.2f} ∠ {angIc:.2f}° A\n")
 
 #Encontrando Vc e IC em Z1
-print("o=> Tensão e corrente na carga Z1:")
+print("o=> Tensão e corrente em rms na carga Z1:")
 Vg = 69e+3
 Vc = (Vg*Zc1)/(matriz2[0][0]*Zc1 + matriz2[0][1])
 Ic = Vc/ Zc1
@@ -207,7 +207,7 @@ print(f"Vcarga1 = {absVc:.2f} ∠ {angVc:.2f}° V")
 print(f"Icarga1 = {absIc:.2f} ∠ {angIc:.2f}° A\n")
 
 #Encontrando Vc e IC em Z2
-print("o=> Tensão e corrente na carga Z2")
+print("o=> Tensão e corrente em rms na carga Z2")
 Vg = 69e+3
 Vc = (Vg*Zc2)/(matriz5[0][0]*Zc2 + matriz5[0][1])
 Ic = Vc/Zc2
